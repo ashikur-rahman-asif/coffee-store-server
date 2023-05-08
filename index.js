c5e0c81fb98efa30f09081a31cd000ea8924b525
asif247 <86229363+asif247@users.noreply.github.com>
2023-05-09 01:13:01 +0600

    steps added

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    //30 to 36 1st step
     const coffeeCollection = client.db("coffeeDB").collection('coffee');
     app.post('/coffee', async (req, res) => {
       const newCoffee = req.body;
